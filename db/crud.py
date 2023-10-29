c342b2332015ea5cc6b0c86a38be11275c3550d5
Ali Habesh <shadi97h@outlook.com>
2023-10-29 23:59:29 +0100

    added more crud operations for bugs and changed get_db() location

--- db/crud.py
+++ db/crud.py
@@ -1,3 +1,4 @@
+from psycopg2 import IntegrityError
 from sqlalchemy.orm import Session
 
 from dtos.dto import BugData
@@ -10,3 +11,24 @@ def create_bug(db: Session, bug: BugData):
     db.commit()
     db.refresh(new_bug)
     return new_bug
+
+
+def update_bug(db: Session, bug_id: int, updated_bug: BugData):
+    try:
+        db.query(Bug).filter(Bug.id == bug_id).update(vars(updated_bug))
+        db.commit()
+        existing_bug = db.query(Bug).get(bug_id)
+        return existing_bug
+    except IntegrityError:
+        db.rollback()
+        return None
+
+
+def delete_bug(db: Session, bug_id: int):
+    bug_to_delete = db.query(Bug).filter(Bug.id == bug_id).first()
+    if bug_to_delete:
+        db.delete(bug_to_delete)
+        db.commit()
+        return True  # Return True to indicate successful deletion
+    else:
+        return False  # Return False if bug with the given ID doesn't exist
--- main.py
+++ main.py
@@ -1,30 +1,12 @@
-# This is a sample Python script.
 from typing import Annotated
-
-# Press Skift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-from fastapi import FastAPI, Depends, Body
-from dtos.dto import BugData
-
-from db.db_setup import SessionLocal
+from fastapi import FastAPI, Depends
+from db.db_setup import  get_db
 from sqlalchemy.orm import Session
-from db import crud
+from api import bug_api
+
 
 app = FastAPI()
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 db_dependency = Annotated[Session, Depends(get_db)]
 
-
-@app.post("/bugs/")
-def create_bug(bug: Annotated[BugData, Body(embed=True)], db: Session = Depends(get_db)):
-    return crud.create_bug(db, bug)
+app.include_router(bug_api.router)
